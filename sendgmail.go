4e4cdfe4a16619b3add8f14b7c9ad753338934cf
SeongJae Park <sj38.park@gmail.com>
2018-05-02 21:34:16 +0900

    Initial commit
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ sendgmail.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"net/smtp"
+	"strings"
+)
+
+type account struct {
+	Username string
+	Password string
+}
+
+type mailcontent struct {
+	Recipients	string
+	Subject		string
+	Message		string
+}
+
+const gmailAccountFile = "gmailinfo"
+const gmailContentFile = "mailcontent"
+
+var gmailAccount account
+var gmailContent mailcontent
+
+func read_gmailinfo() {
+	c, err := ioutil.ReadFile(gmailAccountFile)
+	if err != nil {
+		fmt.Printf("failed to read mail info file: %s\n", err)
+		return
+	}
+	if err := json.Unmarshal(c, &gmailAccount); err != nil {
+		fmt.Printf("failed to unmarshal mail info: %s\n", err)
+		return
+	}
+}
+
+func save_gmailinfo() {
+	bytes, err := json.Marshal(gmailAccount)
+	if err != nil {
+		fmt.Printf("failed to marshal account: %s\n", err)
+		return
+	}
+
+	if err := ioutil.WriteFile(gmailAccountFile, bytes, 0600); err != nil {
+		fmt.Printf("failed to write account info: %s\n", err)
+		return
+	}
+}
+
+func read_gmailContent() {
+	c, err := ioutil.ReadFile(gmailContentFile)
+	if err != nil {
+		fmt.Printf("failed to read mail content file: %s\n", err)
+		return
+	}
+	if err := json.Unmarshal(c, &gmailContent); err != nil {
+		fmt.Printf("failed to unmarshal mail info: %s\n", err)
+		return
+	}
+}
+
+func sendgmail(sender string, receipients, subject, message string) {
+	username := gmailAccount.Username
+	password := gmailAccount.Password
+	if username == "" || password == "" {
+		fmt.Printf("Mail info not read\n")
+		return
+	}
+	hostname := "smtp.gmail.com"
+	port := 587
+	auth := smtp.PlainAuth("", username, password, hostname)
+	msg := fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s\r\n", receipients,
+		subject, message)
+	err := smtp.SendMail(
+		fmt.Sprintf("%s:%d", hostname, port),
+		auth, sender, strings.Split(receipients, ", "), []byte(msg))
+	if err != nil {
+		fmt.Printf("failed to send message: %s\n", err)
+	}
+}
+
+func main() {
+	read_gmailinfo()
+	read_gmailContent()
+	sendgmail("s2j2", gmailContent.Recipients, gmailContent.Subject,
+		gmailContent.Message)
+	save_gmailinfo()
+}
